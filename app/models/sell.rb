e1c509a48130fb0194db291314fb4828bbba7b59
CaioFML <caio.felipemotta@hotmail.com>
2020-03-06 15:38:20 -0300

    Customizando o Comportamento

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,7 +1,13 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
+  enum status: { finished: 0, canceled: 1 }
+
   def fae_display_field
-    
+    id
+  end
+
+  def self.for_fae_index
+    order(:id)
   end
 
   belongs_to :discount
