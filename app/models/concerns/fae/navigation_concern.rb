8c5d4bc89f0a9774ddba08078fed37b201dbfcdd
CaioFML <caio.felipemotta@hotmail.com>
2020-03-10 21:35:35 -0300

    correcao

--- app/models/concerns/fae/navigation_concern.rb
+++ app/models/concerns/fae/navigation_concern.rb
@@ -31,7 +31,7 @@ module Fae
           item('Suppliers', path: admin_suppliers_path),
           item('Services', path: admin_services_path),
           item('Products', path: admin_products_path),
-          item('Sells', path: admin_sells_path),
+          item('Sells', path: admin_sells_path)
         ]),
         item('Marketing', subitems: [
           item('Campaigns', path: admin_campaigns_path)
