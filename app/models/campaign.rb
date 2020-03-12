4eb4b6f8c57b2672e9de742c5ca4d3f10d8dd8b9
CaioFML <caio.felipemotta@hotmail.com>
2020-03-11 21:55:51 -0300

    Atualizando a campanha e atualizando a campanha

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,13 +1,21 @@
 class Campaign < ApplicationRecord
   include Fae::BaseModelConcern
-
-  has_many :campaign_clients
-  has_many :clients, through: :campaign_clients
-
+ 
   validates :title, presence: true
   validates :body, presence: true
-
+  has_many :campaign_clients
+  has_many :clients, through: :campaign_clients
+ 
+  after_save :schedule_emails
+ 
   def fae_display_field
     title
+  end
+ 
+  def schedule_emails
+    Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaingJob.perform_later client, self.title, self.body
+    end
   end
 end
